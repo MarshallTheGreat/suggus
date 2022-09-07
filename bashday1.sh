5eea927c348cbd83901b21001050f871034f8a6c
MarshallTheGreat <112420817+MarshallTheGreat@users.noreply.github.com>
2022-09-07 09:59:50 -0400

    Create bashday1.sh

---
+++ bashday1.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+Getting Help (man pages)
+  man pages exist for every bash command
+  man -k can search for a command with key word
+  curl cht.sh/(command) displays a list of examples for the given bash command
+  
+Online Resources
+  
+
+Working in Bash (Command Line Nav)
+Linux File System (Absolute/Relative/Home file references)
+File Manipulation Commands
+Link Files (Symbolic vs. Hard Symbolic Link)
+Viewing File Contents
+Finding Files and Directories
+Command: grep
+Brace Expansion
+Processes and Memory
+Command: cut
+Command Chaning Operators
+Redirection Operators
+Activities 1-5

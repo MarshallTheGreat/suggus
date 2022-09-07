5157054cfd93a449e7cd97ecdc0c85d38297faf9
MarshallTheGreat <112420817+MarshallTheGreat@users.noreply.github.com>
2022-09-07 16:15:07 -0400

    Update bashday1.sh

--- bashday1.sh
+++ bashday1.sh
@@ -1,23 +1,31 @@
 #!bin/bash
 
-Getting Help (man pages)
+'Getting Help (man pages)'
   man pages exist for every bash command
   man -k can search for a command with key word
+  man find
   curl cht.sh/(command) displays a list of examples for the given bash command
   
-Online Resources
+'Online Resources'
+  explainshell.com - breaks down commands and the related options
+  tldp.org/LDP/abs/html/index.html - In depth explanations and good examples. (Show "Special Characters" and "Loops" as examples)
+  bit.ly/USACYSscripting - Google Drive which includes helpful examples. (Show AWK)
+  gnu.org/software/bash/manual/ - For advanced students who want to know how BASH works in the background.
+  linuxize.com/post/bash-if-else-statement/ - Good examples of if/else statements.
   
 
-Working in Bash (Command Line Nav)
-Linux File System (Absolute/Relative/Home file references)
-File Manipulation Commands
-Link Files (Symbolic vs. Hard Symbolic Link)
-Viewing File Contents
-Finding Files and Directories
-Command: grep
-Brace Expansion
-Processes and Memory
-Command: cut
-Command Chaning Operators
-Redirection Operators
-Activities 1-5
+'Working in Bash (Command Line Nav)'
+  
+
+'Linux File System (Absolute/Relative/Home file references)'
+'File Manipulation Commands'
+'Link Files (Symbolic vs. Hard Symbolic Link)'
+'Viewing File Contents'
+'Finding Files and Directories'
+'Command: grep'
+'Brace Expansion'
+'Processes and Memory'
+'Command: cut'
+'Command Chaning Operators'
+'Redirection Operators'
+'Activities 1-5'
